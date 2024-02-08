ac78e41919df03b10fb2991bbd36869b682a6616
Emanuel <emaa.forlin@gmail.com>
2024-02-08 01:01:58 -0300

    Adds database

---
+++ database/postgresDatabase.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/emaforlin/banking-api/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type postgresDatabase struct {
+	Db *gorm.DB
+}
+
+func NewPosgresDatabase(cfg *config.Config) Database {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=%s",
+		cfg.Db.Host,
+		cfg.Db.User,
+		cfg.Db.Passwd,
+		cfg.Db.DBname,
+		cfg.Db.Port,
+		cfg.Db.SSLMode,
+		cfg.App.TimeZone,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return &postgresDatabase{Db: db}
+}
+
+func (p *postgresDatabase) GetDb() *gorm.DB {
+	return p.Db
+}
