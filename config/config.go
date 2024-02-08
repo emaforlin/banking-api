c254a503db991aa7472204f4d70f166f77108b56
Emanuel <emaa.forlin@gmail.com>
2024-02-08 00:57:01 -0300

    add config management

---
+++ config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	App App
+	Db  Db
+}
+
+type App struct {
+	Port     uint16
+	TimeZone string
+}
+
+type Db struct {
+	Host    string
+	Port    uint16
+	User    string
+	Passwd  string
+	DBname  string
+	SSLMode string
+}
+
+func GetConfig() Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal err reading config file: %v", err))
+	}
+
+	return Config{
+		App: App{
+			Port:     viper.GetUint16("app.server.port"),
+			TimeZone: viper.GetString("app.tz"),
+		},
+		Db: Db{
+			Host:    viper.GetString("database.host"),
+			Port:    viper.GetUint16("database.port"),
+			User:    viper.GetString("database.user"),
+			Passwd:  viper.GetString("database.passwd"),
+			DBname:  viper.GetString("database.dbname"),
+			SSLMode: viper.GetString("database.sslmode"),
+		},
+	}
+}
