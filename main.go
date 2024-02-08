0201956db0a69638a7118435fe5e7aa44649af47
Emanuel <emaa.forlin@gmail.com>
2024-02-08 01:47:00 -0300

    Adds the entrypoint

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/emaforlin/banking-api/config"
+	"github.com/emaforlin/banking-api/database"
+	"github.com/emaforlin/banking-api/server"
+)
+
+func main() {
+	cfg := config.GetConfig()
+	database := database.NewPosgresDatabase(&cfg)
+	server.NewGinServer(&cfg, database.GetDb()).Start()
+}
